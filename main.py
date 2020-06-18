cc83c781de80b9161bd320ce61b968d4f2892b17
partypigcool <annaritagi@hotmail.com>
2020-06-18 17:48:06 +0000

    i made movement so we can now move the ship, still no astoroid

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 from pygame.locals import *
 import sys
-
+from ship import Ship
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -12,9 +12,45 @@ screen_size = (width, height) = (int(screen_info.current_w), int(screen_info.cur
 screen = pygame.display.set_mode(screen_size)
 color = (0, 127, 255)
 clock = pygame.time.Clock()
+
+
+player = Ship((20, 200))
+
+# setup game variables
+numlevels = 4
+level = 1
+
+
+
+
+
+
 def main():
-  clock.tick(60)
-  screen.fill(color)
-  pygame.display.flip()
+  global Level, NumLevels
+  while level <= numlevels:
+    clock.tick(60)
+    for event in pygame.event.get():
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_d:
+          player.speed[0] = 10
+        if event.key == pygame.K_a:
+          player.speed[0] = -10
+        if event.key == pygame.K_w:
+          player.speed[1] = -10
+        if event.key == pygame.K_s:
+          player.speed[1] = 10
+      if event.type == pygame.KEYUP:
+        if event.key == pygame.K_d:
+          player.speed[0] = 0
+        if event.key == pygame.K_a:
+          player.speed[0] = 0
+        if event.key == pygame.K_w:
+          player.speed[1] = 0
+        if event.key == pygame.K_s:
+          player.speed[1] = 0
+    player.update()
+    screen.fill(color)
+    screen.blit(player.image, player.rect)
+    pygame.display.flip()
 if __name__ == '__main__':
   main()
--- ship.py
+++ ship.py
@@ -1,3 +1,32 @@
 import pygame
 
-class Ship(pygame.sprite.Sprite)
+class Ship(pygame.sprite.Sprite):
+  def __init__(self, pos):
+    super().__init__()
+    self.image = pygame.image.load("ship.png")
+    self.scale =  1
+    width = int(self.image.get_rect().width*self.scale)
+    height = int(self.image.get_rect().height*self.scale)
+    self.image = pygame.transform.smoothscale(self.image, (width, height))
+    self.image = pygame.transform.rotate(self.image, -90)
+    self.rect = self.image.get_rect()
+    self.rect.center = pos
+    self.speed = pygame.math.Vector2(2,0)
+
+  def update(self):
+    self.rect.move_ip(self.speed)
+    screen_info = pygame.display.Info()
+    if self.rect.right > screen_info.current_w:
+      self.rect.right = screen_info.current_w
+    if self.rect.left < 0:
+      self.rect.left = 0
+    if self.rect.bottom > screen_info.current_h:
+      self.rect.bottom = screen_info.current_h
+    if self.rect.top < 0:
+      self.rect.top = 0
+
+  def checkReset(self, endPos):
+    return self.rect.right >= endPos
+
+  def reset(self, pos):
+    self.rect.center = pos
